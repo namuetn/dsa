1148bc3bd21010384d0800151fe4d3a62e223491
Kieu Thanh Nam <nam.uetn@gmail.com>
2023-12-25 17:36:46 +0700

    first commit

---
+++ dsa_module1.py
@@ -0,0 +1,20 @@
+
+# lesson 1
+lesson_1 = input()
+print(lesson_1)
+
+# lesson 2
+lesson_2 = 'Hello world'
+print(lesson_2)
+
+# lesson 3
+lesson_3_1 = 'hello'
+lesson_3_2 = 'world'
+lesson_3 = lesson_3_1 + ' ' + lesson_3_2
+print(lesson_3)
+
+# lesson 4
+def print_hello_world(data):
+    print(data)
+
+print_hello_world(lesson_3)
